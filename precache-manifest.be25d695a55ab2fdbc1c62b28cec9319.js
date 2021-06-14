2190897a8317175cdddd4f8a7b9537f39be965df
dghodse <ghodsednyaneshwar@gmail.com>
2021-06-14 09:25:30 +0530

    Updates

---
+++ precache-manifest.be25d695a55ab2fdbc1c62b28cec9319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fdc523d4868cb05a0e5b860c981fab5",
+    "url": "/React-review/index.html"
+  },
+  {
+    "revision": "7cd8891fe798bf9aa3a3",
+    "url": "/React-review/static/css/main.fd227e3e.chunk.css"
+  },
+  {
+    "revision": "e09e314e86a1b74af0fb",
+    "url": "/React-review/static/js/2.b58b8770.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-review/static/js/2.b58b8770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cd8891fe798bf9aa3a3",
+    "url": "/React-review/static/js/main.db325fab.chunk.js"
+  },
+  {
+    "revision": "63adde4ec8178e100202",
+    "url": "/React-review/static/js/runtime-main.5dde0b63.js"
+  }
+]);
